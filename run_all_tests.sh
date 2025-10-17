a701ea6d5f6bd14ec1f4aa6894a1a705c9764017
Shahar Sandhaus <wiishahar@gmail.com>
2025-10-17 16:21:35 -0700

    Working on scripts to auto download and run nvidia tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+mkdir -p test_results
+
+#cd test_results
+
 #DATA_SIZE=134217728
 DATA_SIZE=67108864
 #DATA_SIZE=33554432
@@ -8,11 +12,13 @@ BATCH_SIZE=10
 REPEATS=3
 ARCH=86
 
-cd fft_nonstrided
-bash run_fft_nonstrided_test.sh $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS $ARCH
-cd ..
+# cd fft_nonstrided
+# bash run_fft_nonstrided_test.sh $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS $ARCH
+# cd ..
+cp fft_nonstrided/test_results/fft_nonstrided_graph.png test_results/fft_nonstrided_graph.png
 
 #cd convolution_nonstrided_scaled
 #bash run_convolution_nonstrided_scaled_test.sh $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS $ARCH
 #cd ..
-
+cp convolution_nonstrided_scaled/test_results/convolution_nonstrided_scaled_graph.png test_results/convolution_nonstrided_scaled_graph.png
+cp convolution_nonstrided_scaled/test_results/convolution_nonstrided_scaled_ratios_graph.png test_results/convolution_nonstrided_scaled_ratios_graph.png
