2cbf006be93d342d167431defe7df1b8d19d3cd3
sharhar <wiishahar@gmail.com>
2025-12-01 22:14:43 +0000

    Fix

--- unlock_gpus.sh
+++ unlock_gpus.sh
@@ -5,8 +5,5 @@ sudo nvidia-smi -rgc
 # 2. Reset Memory Clock to default behavior
 sudo nvidia-smi -rmc
 
-# 3. Reset Power Limit (if modified)
-sudo nvidia-smi -rpl
-
-# 4. Disable Persistence Mode
+# 3. Disable Persistence Mode
 sudo nvidia-smi -pm 0
